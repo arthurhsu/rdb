545fb54c285e9ad241a2efd73fc1e8b6b7744052
Arthur Hsu <arthurhsu@chromium.org>
2016-01-06 14:26:50 -0800

    Fix TOC issues, started real work

---
+++ examples/schema/create-by-json.js
@@ -0,0 +1,44 @@
+// Create a database using predefined JSON schema.
+var hrSchema = {
+  'name': 'hr',
+  'table': [
+    {
+      'name': 'Dept',
+      'column': [
+        {'name': 'id', 'type': 'String'},
+        {'name': 'name', 'type': 'String'}
+      ],
+      'constraint': {
+        'primaryKey': [{'name': 'id'}],
+        'notNull': ['name']
+      }
+    },
+    {
+      'name': 'Emp',
+      'column': [
+        {'name': 'id', 'type': 'Number'},
+        {'name': 'name', 'type': 'String'},
+        {'name': 'deptId', 'type': 'String'},
+        {'name': 'title', 'type': 'String'}
+      ],
+      'constraint': {
+        'primaryKey': [{'name': 'id', 'order': 'desc'}],
+        'foreignKey': [{
+          'name': 'fk_DeptId',
+          'local': 'deptId',
+          'remote': 'Dept.id',
+          'action': 'restrict',
+          'timing': 'immediate'
+        }],
+        'unique': ['name'],
+        'notNull': ['id', 'name', 'deptId']
+      }
+    }
+  ]
+};
+
+db.create(hrSchema).exec().then(function(instance) {
+  // Instance is the connected instance.
+}, function(e) {
+  // e is the DOMError if any, e.g. AlreadyExists.
+});
