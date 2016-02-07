9f4829a09685b4784f5da969525160faea34e0ee
arthurhsu <artchsu@gmail.com>
2016-02-06 20:43:19 -0800

    Complete rearrange how docs organize.
    The re-spec imposes many restrictions and the best way to work around it
    is to make sure the output is one big HTML. As a result, created some gulp
    scripts to make sure that can be done, while keep the document structure
    organized.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var fs = require('fs-extra');
 var gulp = require('gulp');
 var gulpConnect = require('gulp-connect');
 var nopt = require('nopt');
@@ -9,8 +10,46 @@ gulp.task('default', function() {
   log('      Start a debug server (default is test at port 8000)');
 });
 
+function getOutputPath() {
+  return path.resolve(path.join(__dirname, 'out'));
+}
 
-gulp.task('debug', function() {
+function copyLines(lines, contents, baseDir) {
+  lines.forEach(function(line) {
+    if (line.match(/{{.*?}}/)) {
+      expandLine(line, contents, baseDir);
+    } else {
+      contents.push(line);
+    }
+  });
+}
+
+function expandLine(line, contents, baseDir) {
+  var includeFile = path.resolve(path.join(
+        baseDir, 
+        line.match(/{{include:(.*?)}}/)[1].trim()));
+  var lines = fs.readFileSync(includeFile, 'utf8').split('\n');
+  copyLines(lines, contents, path.dirname(includeFile));
+}
+
+function generateIndex() {
+  console.log('Regenerating index.html ...');
+  fs.ensureDirSync(getOutputPath());
+  var lines = fs.readFileSync(path.resolve(
+        path.join(__dirname, 'contents/main.html')), 'utf8').split('\n');
+  var contents = [];
+  copyLines(lines, contents, path.resolve(path.join(__dirname, 'contents')));
+  fs.writeFileSync(
+      path.resolve(path.join(__dirname, 'out/index.html')),
+      contents.join('\n'),
+      'utf8');
+}
+
+gulp.task('watch', function() {
+  gulp.watch('contents/**/*').on('change', generateIndex);
+});
+
+gulp.task('debug', ['watch'], function() {
   var knownOpts = {
     'port': [Number, null]
   };
@@ -18,9 +57,10 @@ gulp.task('debug', function() {
   var options = nopt(knownOpts);
   var port = options.port || 8000;
 
+  generateIndex();
   gulpConnect.server({
     livereload: true,
     port: port,
-    root: path.resolve(__dirname)
+    root: getOutputPath()
   });
 });
