d5c2a32d27b84139dfb135fda930066befc16b8b
Arthur Hsu <arthurhsu@google.com>
2017-07-24 15:11:48 -0700

    Update upgrade_db.js to meet latest design

--- spec/example/upgrade_db.js
+++ spec/example/upgrade_db.js
@@ -1,30 +1,26 @@
 let dbConnection;
 let version = dbConnection.schema().version;
 if (version < 2) {
-    // Need DB upgrade.
-    let tx = dbConnection.createTransaction('readwrite');
-    // BEGIN TRANSACTION equivalent in RDB.
-    tx.begin().then(() => {
-        return tx.attach(dbConnection.setVersion(2));
-    }).then(() => {
-        let q = dbConnection
-            .createTable('NewTable')
-            .column('id', 'string', true)
-            .column('name', 'string', true);
-        return tx.attach(q);
-    }).then(() => {
-        let q = dbConnection
-            .alterTable('Emp')
-            .addColumn('location', 'string', true, 'LAX')
-            .setColumn('title').set('title', true) // Change to NOT NULL
-            .addIndex('idx_location', 'location');
-        return tx.attach(q);
-    }).then(() => {
-        return tx.attach(dbConnection.dropTable('Foo'));
-    }).then(() => {
-        // COMMIT current transaction.
-        return tx.commit();
-    }).then(() => {
+  // Need DB upgrade.
+  let tx = dbConnection.createTransaction('readwrite');
+  let setVersion = dbConnection.setVersion(2);
+  let createNewTable = dbConnection
+      .createTable('NewTable')
+      .column('id', 'string', true)
+      .column('name', 'string', true);
+    
+  let alterTable = dbConnection
+      .alterTable('Emp')
+      .addColumn('location', 'string', true, 'LAX')
+      .setColumn('title').set('title', true) // Change to NOT NULL
+      .addIndex('idx_location', 'location');
+
+  let dropTable = dbConnection.dropTable('Foo');
+
+  // MUST use exec() instead of attach().
+  return tx
+      .exec([setVersion, createNewTable, alterTable, dropTable])
+      .then(() => {
         // Schema change has finished, start work here.
-    });
+      });
 }
